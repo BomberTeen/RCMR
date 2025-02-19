b423cbe90ad10718971fbc07d50bfe0616a6730e
Bomber <140200643+BomberTeen@users.noreply.github.com>
2025-02-19 14:56:06 -0600

    Create prueba de interfaz.py

---
+++ prueba de interfaz.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import cv2
+ventana = tk.Tk()
+
+boton = tk._ButtonCommand
+
+ventana.geometry("800x600")
+ventana.configure(bg = "Purple")Logitech_BRIO
+etiqueta = Tkinter.Labelframe
+
+def init_ui(self)
+    self.parent.title("Cloud Interface")
+ventana.mainloop()
+

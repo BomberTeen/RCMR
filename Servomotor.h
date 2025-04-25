4ea604d69c5150e7a9fbe5f3ce1072b901ac251a
MarLya4 <marthalopezresearch@gmail.com>
2025-04-25 15:18:31 -0600

    Update Servomotor.h

--- Servomotor.h
+++ Servomotor.h
@@ -1,69 +1,142 @@
-#ifndef Servomotor_h
-#define Servomotor_h
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
-#include <Servo.h>
-#include <Arduino.h>
+Adafruit_PWMServoDriver pca9685 = Adafruit_PWMServoDriver();
+ 
 
-class Servomotor{
-    public:
-    Servo servo;
-    unsigned long Actual;
-    unsigned long Delay = 11500;
-    
-    unsigned long last;
-    int pos;
-    int pin;
+class Temporizador {
+  private:
+    unsigned long tiempo;
+    unsigned long inicial = millis();
 
-    Servomotor (){
-      pos = 90;
-      last = 0;
-      servo.write(pos);
-    }  
-
-  void set(int pin){
-    servo.attach(pin);
-    servo.write(pos);
-  }
-
-  void SetPosition(int Prepos){
-      while(pos != Prepos){
-        Actual = micros();
-        if (Actual - last >= Delay){
-          if (pos < Prepos) pos++;
-          else if (pos > Prepos) pos--;
-
-        }
-      }
-  }
-
-
-  void aumentar(){
-    Actual = micros();
-    if (Actual - last >= Delay){
-      last = Actual;
-      if (pos < 180) pos++;
-      servo.write(pos);
+  public:
+    Temporizador(unsigned long tiempo) {
+      this->tiempo = tiempo;
+      inicial = millis();
     }
 
-  }
-
-  void disminuir(){
-    Actual = micros();
-    if (Actual - last >= Delay){
-      last = Actual;
-      if(pos > 0) pos--;
-      servo.write(pos);
+    long faltante() {
+      return tiempo + inicial - millis();
     }
 
-  }
+    bool termino() {
+      return faltante() <= 0;
+    }
 
+    void reiniciar() {
+      inicial = millis();
+    }
 
-void abrir(){
-  servo.write(180);
-}
-
-void cerrar(){
-  servo.write(0);
-}
+    void cambiar(long nuevo) {
+      tiempo = nuevo;
+    }
 };
-#endif
+
+
+class ServoControl{
+  private:
+    static Adafruit_PWMServoDriver pca9685;
+    
+    unsigned int canal;
+    unsigned long intervalo;
+    Temporizador timer = NULL;
+    
+  public:
+  unsigned int MAX = 512;
+  unsigned int MIN = 102;
+  unsigned int pulso;
+  unsigned int actual;
+  ServoControl(unsigned int canal, unsigned int pulso, unsigned long intervalo){
+    this->canal = canal;
+    this->pulso = pulso;
+    this->actual = pulso;
+    timer = Temporizador(intervalo);
+  }
+
+  void inciar(){
+    pca9685.setPWM(canal, 0, MIN); 
+  }
+ 
+  static void iniciardriver() {
+      pca9685.begin();
+      pca9685.setPWMFreq(50);
+    }
+ 
+  void stop() {
+      pulso = actual;
+    }
+
+    void update() {
+      if (actual == pulso) return;
+      if (!timer.termino()) return;
+      timer.reiniciar();
+      if (actual > pulso) {actual = actual- 3;}
+      else actual = actual + 3;
+
+      pca9685.setPWM(canal, 0, actual);
+    }
+
+
+   void cambiar_pulso(unsigned int nuevo_pulso){
+    pulso = nuevo_pulso;
+  }
+
+  void cambiar_velocidad( unsigned long nueva_velocidad){
+    intervalo = nueva_velocidad;
+  }
+
+};
+unsigned int MAX1 = 512;
+unsigned int MIN1 = 102;
+
+Adafruit_PWMServoDriver ServoControl::pca9685;
+ServoControl a = ServoControl(0, 102, 20);
+ServoControl b = ServoControl(1, 102, 20);
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Conexion establecida");
+
+  ServoControl::iniciardriver();
+  a = ServoControl(0, 102, 20);
+  b = ServoControl(1, 102, 20);
+  a.inciar();
+  b.inciar();
+  //b.cambiar_velocidad(40);
+  //delay(5000);
+}
+void loop(){
+  a.update();
+  b.update();
+  
+  if (Serial.available() > 0){
+    char currentCommand = Serial.read();
+   
+    switch(currentCommand){
+
+    case 'z':
+    a.cambiar_pulso(MAX1);
+    break;
+
+    case 'x':
+    a.cambiar_pulso(MIN1);
+    break;
+
+    case 'c':
+    b.cambiar_pulso(MIN1);
+    break;
+
+    case 'v':
+    b.cambiar_pulso(MAX1);
+    break;
+
+    case 'p':
+    b.stop();
+    a.stop();
+    break;
+    }
+
+   
+  }
+ 
+}
